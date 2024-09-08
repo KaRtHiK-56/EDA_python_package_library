101e634003d07cf4c6bf0364c9fc31a101fca941
Karthik-56 <karthiksurya611@gmail.com>
2024-09-08 20:52:39 +0530

    adding files

--- setup.py
+++ setup.py
@@ -1,29 +1,8 @@
 from setuptools import setup, find_packages
 
-"""with open(r"C:\Users\Devadarsan\Desktop\Karthik_projects\EDA_Package\EDA_python_package_library\README.md", "r") as f:
-    long_description = f.read()
 
-setup(
-    name="EDA-Python-Library",
-    version="0.0.1",
-    description="A Library for Making the Explorartory Data Analysis process easy in single line of codes.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/KaRtHiK-56/EDA_python_package_library",
-    author="Karthik",
-    author_email="karthiksurya611@gmail.com",
-    license="MIT",
-    classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.10",
-        "Operating System :: OS Independent",
-    ],
-    install_requires=["bson >= 0.5.10"],
-    extras_require={
-        "dev": ["pytest>=7.0", "twine>=4.0.2"],
-    },
-    python_requires=">=3.10",
-)"""
+with open(r"C:\Users\Devadarsan\Desktop\Karthik_projects\EDA_Package\EDA_python_package_library\README.md", "r") as f:
+    long_description = f.read()
 
  
 classifiers = [
@@ -31,14 +10,14 @@ classifiers = [
   'Intended Audience :: Education',
   'Operating System :: Microsoft :: Windows :: Windows 10',
   'License :: OSI Approved :: MIT License',
-  'Programming Language :: Python :: 3.10.0'
+  'Programming Language :: Python :: 3.10'
 ]
  
 setup(
-  name='EDA-Python-Library',
-  version='0.0.2',
+  name='edapython',
+  version='0.0.2.3',
   description='A Library for Making the Explorartory Data Analysis process easy in single line of codes',
-  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  long_description=long_description,
   url="https://github.com/KaRtHiK-56/EDA_python_package_library",  
   author='Karthik',
   author_email='karthiksurya611@gmail.com',
