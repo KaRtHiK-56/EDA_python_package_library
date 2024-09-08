f524a3a17e64d352dd304297527ae8a26ab8b330
Karthik-56 <karthiksurya611@gmail.com>
2024-09-08 13:32:54 +0530

    adding files

---
+++ eda.py
@@ -0,0 +1,10 @@
+#importing necessary libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt 
+import seaborn as sns 
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder,StandardScaler,RobustScaler
+from scipy import stats 
+from scipy.stats import skewness,kurtosis
+
+
