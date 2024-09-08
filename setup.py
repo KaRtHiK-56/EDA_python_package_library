9533ad2309f65c07c72493424392b1f2e841582e
Karthik-56 <karthiksurya611@gmail.com>
2024-09-08 20:35:52 +0530

    adding files

--- setup.py
+++ setup.py
@@ -1,37 +1,50 @@
 from setuptools import setup, find_packages
-import codecs
-import os
 
-here = os.path.abspath(os.path.dirname(__file__))
+"""with open(r"C:\Users\Devadarsan\Desktop\Karthik_projects\EDA_Package\EDA_python_package_library\README.md", "r") as f:
+    long_description = f.read()
 
-with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
-    long_description = "\n" + fh.read()
-
-VERSION = '0.0.2'
-DESCRIPTION = 'Package/Library to do Exploratory Data Analysis'
-LONG_DESCRIPTION = 'A package that allows to build Exploratory Data Analysis just by passing Dataset and column names.'
-
-def get_requirements(file_path: str):
-    """
-    this function will return the list of libraries in the requirements.txt file
-
-    """
-    requirements = []
-    with open(file_path) as file_obj:
-        requirements = file_obj.readlines()
-        requirements = [req.replace("\n", "") for req in requirements]
-
-    return requirements
-
-# Setting up
 setup(
-    name="Exploratory-Data-Analysis",
-    version=VERSION,
-    author="Karthik",
-    author_email="<karthiksurya611@gmail.com>",
-    description=DESCRIPTION,
-    long_description_content_type="text/markdown",
+    name="EDA-Python-Library",
+    version="0.0.1",
+    description="A Library for Making the Explorartory Data Analysis process easy in single line of codes.",
     long_description=long_description,
-    packages=find_packages(),
-    install_requires=get_requirements("requirements.txt"),
+    long_description_content_type="text/markdown",
+    url="https://github.com/KaRtHiK-56/EDA_python_package_library",
+    author="Karthik",
+    author_email="karthiksurya611@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["bson >= 0.5.10"],
+    extras_require={
+        "dev": ["pytest>=7.0", "twine>=4.0.2"],
+    },
+    python_requires=">=3.10",
+)"""
+
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3.10.0'
+]
+ 
+setup(
+  name='EDA-Python-Library',
+  version='0.0.2',
+  description='A Library for Making the Explorartory Data Analysis process easy in single line of codes',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url="https://github.com/KaRtHiK-56/EDA_python_package_library",  
+  author='Karthik',
+  author_email='karthiksurya611@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='eda', 
+  packages=find_packages(),
+  install_requires=[''] 
 )
